b0dec3915ca5495e7e7135f02c34cda73bc89656
Soby Chacko <schacko@pivotal.io>
2020-05-08 20:36:01 -0400

    Groovy transform app build

--- src/main/groovy/org/springframework/jenkins/stream/applications/common/StreamApplicationsCommons.groovy
+++ src/main/groovy/org/springframework/jenkins/stream/applications/common/StreamApplicationsCommons.groovy
@@ -16,13 +16,14 @@ class StreamApplicationsCommons {
                                                            'transform-processor':'processor/transform-processor', 'jdbc-sink':'sink/jdbc-sink',
                                                            'counter-sink':'sink/counter-sink', 'bridge-processor':'processor/bridge-processor',
                                                            'file-sink':'sink/file-sink', 'file-source':'source/file-source',
-                                                           'redis-sink':'sink/redis-sink', 'header-enricher-processor':'processor/header-enricher-processor']
+                                                           'redis-sink':'sink/redis-sink', 'header-enricher-processor':'processor/header-enricher-processor',
+                                                           'groovy-transform-processor':'processor/groovy-transform-processor']
 
     public static final List<String> PHASE1_KEYS = ['jdbc-source', 'http-source', 'splitter-processor', 'rabbit-sink', 'time-source', 'log-sink', 'cassandra-sink',
                                                     'mongodb-sink', 'mongodb-source', 'filter-processor']
 
     public static final List<String> PHASE2_KEYS = ['transform-processor', 'jdbc-sink', 'counter-sink', 'bridge-processor', 'file-source', 'file-sink',
-                                                    'redis-sink', 'header-enricher-processor']
+                                                    'redis-sink', 'header-enricher-processor', 'groovy-transform-processor']
 
     public static final List<List<String>> ALL_JOBS = [PHASE1_KEYS, PHASE2_KEYS]
 
