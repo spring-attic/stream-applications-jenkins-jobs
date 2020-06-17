89c7b9611da828b3fdc30a307f85839e9a21996d
Soby Chacko <schacko@pivotal.io>
2020-06-17 18:59:31 -0400

    New builds
    FTP Source, JMS Source, Websocket Source/Sink, Tasklauncher Sink

--- src/main/groovy/org/springframework/jenkins/stream/applications/common/StreamApplicationsCommons.groovy
+++ src/main/groovy/org/springframework/jenkins/stream/applications/common/StreamApplicationsCommons.groovy
@@ -11,15 +11,18 @@ class StreamApplicationsCommons {
     public static final Map<String, String> PHASED_JOBS = [
             //sources
             'file-source'              : 'source/file-source',
+            'ftp-source'               : 'source/ftp-source',
             'geode-source'             : 'source/geode-source',
             'http-source'              : 'source/http-source',
             'jdbc-source'              : 'source/jdbc-source',
+            'jms-source'               : 'source/jms-source',
             'load-generator-source'    : 'source/load-generator-source',
             'mongodb-source'           : 'source/mongodb-source',
             'mqtt-source'              : 'source/mqtt-source',
             'rabbit-source'            : 'source/rabbit-source',
             'tcp-source'               : 'source/tcp-source',
             'time-source'              : 'source/time-source',
+            'websocket-source'         : 'source/websocket-source',
             //sinks
             'cassandra-sink'           : 'sink/cassandra-sink',
             'counter-sink'             : 'sink/counter-sink',
@@ -35,7 +38,9 @@ class StreamApplicationsCommons {
             'router-sink'              : 'sink/router-sink',
             'sftp-sink'                : 'sink/sftp-sink',
             'tcp-sink'                 : 'sink/tcp-sink',
+            'tasklauncher-sink'        : 'sink/tasklauncher-sink',
             'throughput-sink'          : 'sink/throughput-sink',
+            'websocket-sink'           : 'sink/websocket-sink',
             //processors
             'bridge-processor'         : 'processor/bridge-processor',
             'filter-processor'         : 'processor/filter-processor',
@@ -47,17 +52,17 @@ class StreamApplicationsCommons {
             'transform-processor'      : 'processor/transform-processor',
     ]
 
-    public static final List<String> PHASE1_KEYS = ['file-source', 'geode-source', 'http-source', 'jdbc-source', 'load-generator-source',
-                                                    'mongodb-source', 'mqtt-source', 'rabbit-source', 'tcp-source', 'time-source']
+    public static final List<String> PHASE1_KEYS = ['file-source', 'ftp-source', 'geode-source', 'http-source', 'jdbc-source', 'jms-source','load-generator-source',
+                                                    'mongodb-source', 'mqtt-source', 'rabbit-source']
 
-    public static final List<String> PHASE2_KEYS = ['cassandra-sink', 'counter-sink', 'file-sink', 'ftp-sink', 'geode-sink',
-                                                    'jdbc-sink', 'log-sink', 'mongodb-sink', 'mqtt-sink', 'rabbit-sink']
+    public static final List<String> PHASE2_KEYS = ['time-source', 'tcp-source', 'websocket-source', 'cassandra-sink', 'counter-sink', 'file-sink', 'ftp-sink', 'geode-sink',
+                                                    'jdbc-sink', 'log-sink']
 
-    public static final List<String> PHASE3_KEYS = ['redis-sink', 'router-sink', 'sftp-sink', 'tcp-sink', 'throughput-sink',
-                                                    'bridge-processor', 'filter-processor', 'groovy-processor',
-                                                    'header-enricher-processor', 'http-request-processor']
+    public static final List<String> PHASE3_KEYS = ['mongodb-sink', 'mqtt-sink', 'rabbit-sink', 'redis-sink', 'router-sink', 'sftp-sink', 'tcp-sink',
+                                                    'tasklauncher-sink', 'throughput-sink', 'websocket-sink']
 
-    public static final List<String> PHASE4_KEYS = ['script-processor', 'splitter-processor', 'transform-processor']
+    public static final List<String> PHASE4_KEYS = ['bridge-processor', 'filter-processor', 'groovy-processor',
+                                                    'header-enricher-processor', 'http-request-processor', 'script-processor', 'splitter-processor', 'transform-processor']
 
     public static final List<List<String>> ALL_JOBS = [PHASE1_KEYS, PHASE2_KEYS, PHASE3_KEYS, PHASE4_KEYS]
 
