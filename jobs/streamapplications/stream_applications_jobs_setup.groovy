858d4936441505eaed8e9c443da43ccdbad2555d
Soby Chacko <schacko@pivotal.io>
2021-02-18 14:16:44 -0500

    2020.0.1 Release builds

--- jobs/streamapplications/stream_applications_jobs_setup.groovy
+++ jobs/streamapplications/stream_applications_jobs_setup.groovy
@@ -5,7 +5,7 @@ import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory dsl = this
 
-String releaseType = "" // possible values are - "", milestone or ga
+String releaseType = "ga" // possible values are - "", milestone or ga
 
 // Master CI
 new StreamApplicationsPhasedBuildMaker(dsl).build(false, releaseType)
