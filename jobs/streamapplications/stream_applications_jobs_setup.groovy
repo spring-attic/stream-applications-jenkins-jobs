aeb77ec1fb7eb70c39f772cd64fd788d86a8dd61
Soby Chacko <schacko@pivotal.io>
2021-05-28 13:12:51 -0400

    Build back to snapshot

--- jobs/streamapplications/stream_applications_jobs_setup.groovy
+++ jobs/streamapplications/stream_applications_jobs_setup.groovy
@@ -8,4 +8,4 @@ DslFactory dsl = this
 String releaseType = "" // possible values are - "", milestone or ga
 
 // Master CI
-new StreamApplicationsPhasedBuildMaker(dsl).build(true, "ga")
+new StreamApplicationsPhasedBuildMaker(dsl).build(false, "")
